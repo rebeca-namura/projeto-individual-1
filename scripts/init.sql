e6bcc7801a4f90bf332d97e216b1da6641abc5d1
Rebeca Namura Sbroglio <rebeca.sbroglio@sou.inteli.edu.br>
2025-05-24 16:22:41 -0300

    feat:  bd in init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,35 +1,31 @@
 -- init.sql
 
--- Criar extensão para suportar UUIDs, se ainda não estiver ativada
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- Criar tabela de usuários com UUID como chave primária
-CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS evento (
+id SERIAL PRIMARY KEY,
+evento VARCHAR(150),
+endereço VARCHAR(300),
+data TIMESTAMP,
+FOREIGN KEY (id_programa) REFERENCES programação(id),
+FOREIGN KEY (id_adm) REFERENCES administrador(id),
+FOREIGN KEY (id_participante) REFERENCES participante(id)
 );
-
--- Inserir 20 usuários com nomes e emails aleatórios
-INSERT INTO users (name, email)
-VALUES 
-  ('Alice Smith', 'alice.smith@example.com'),
-  ('Bob Johnson', 'bob.johnson@example.com'),
-  ('Carol Williams', 'carol.williams@example.com'),
-  ('David Jones', 'david.jones@example.com'),
-  ('Emma Brown', 'emma.brown@example.com'),
-  ('Frank Davis', 'frank.davis@example.com'),
-  ('Grace Wilson', 'grace.wilson@example.com'),
-  ('Henry Moore', 'henry.moore@example.com'),
-  ('Isabella Taylor', 'isabella.taylor@example.com'),
-  ('Jack Lee', 'jack.lee@example.com'),
-  ('Kate Clark', 'kate.clark@example.com'),
-  ('Liam Martinez', 'liam.martinez@example.com'),
-  ('Mia Rodriguez', 'mia.rodriguez@example.com'),
-  ('Noah Garcia', 'noah.garcia@example.com'),
-  ('Olivia Hernandez', 'olivia.hernandez@example.com'),
-  ('Patrick Martinez', 'patrick.martinez@example.com'),
-  ('Quinn Lopez', 'quinn.lopez@example.com'),
-  ('Rose Thompson', 'rose.thompson@example.com'),
-  ('Samuel Perez', 'samuel.perez@example.com'),
-  ('Tara Scott', 'tara.scott@example.com');
+CREATE TABLE IF NOT EXISTS administrador(
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100),
+FOREIGN KEY (id_evento) REFERENCES evento(id),
+email VARCHAR(300)
+);
+CREATE TABLE IF NOT EXISTS participante (
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100),
+email VARCHAR(300),
+cpf INT,
+FOREIGN KEY (id_evento) REFERENCES evento(id)
+);
+CREATE TABLE IF NOT EXISTS programação (
+id SERIAL PRIMARY KEY,
+data TIMESTAMP,
+descrição VARCHAR(500),
+FOREIGN KEY (id_evento) REFERENCES evento(id)
+);
